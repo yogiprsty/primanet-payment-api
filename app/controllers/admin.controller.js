1665a04d84391a338dea1022583724008d6a4064
Yogi Prasetyo <prasetyoyogi321@gmail.com>
2022-08-02 17:09:10 +0700

    menambahkan edit dan delete package

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -62,14 +62,71 @@ const createPackage = (req, res) => {
     });
 };
 
-const getAllUsers = async (req, res) => {
-  const users = await User.findAll({
+const getAllPackages = (req, res) => {
+  Package.findAll()
+    .then((result) => (res.send(result)))
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};
+
+const getPackage = (req, res) => {
+  const { id } = req.params;
+  Package.findByPk(id)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};
+
+const updatePackage = (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const internetPackage = { name, price };
+  Package.update(internetPackage, {
+    where: {
+      id,
+    },
+  }).then(() => {
+    res.send({ message: 'Package Updated Successfully' });
+  }).catch((err) => {
+    res.status(500).send({
+      message: err.message,
+    });
+  });
+};
+
+const deletePackage = (req, res) => {
+  const { id } = req.params;
+  Package.destroy({ where: { id } })
+    .then(() => {
+      res.send({ message: 'Package Deleted Successfully' });
+    }).catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};
+
+const getAllUsers = (req, res) => {
+  User.findAll({
     where: {
       isAdmin: 0,
     },
     include: [Package],
-  });
-  res.send(users);
+  })
+    .then((result) => (res.send(result)))
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
 };
 
 const getUser = async (req, res) => {
@@ -86,5 +143,12 @@ const getUser = async (req, res) => {
 };
 
 module.exports = {
-  registerAdmin, createPackage, getAllUsers, getUser,
+  registerAdmin,
+  createPackage,
+  getAllPackages,
+  getPackage,
+  updatePackage,
+  deletePackage,
+  getAllUsers,
+  getUser,
 };
--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const {
   registerAdmin,
   createPackage,
+  getAllPackages,
+  getPackage,
+  updatePackage,
+  deletePackage,
   getAllUsers,
   getUser,
 } = require('../controllers/admin.controller');
@@ -12,10 +16,19 @@ const { verifyToken, isAdmin } = require('../middleware/authJwt');
 const router = express.Router();
 router.post('/register', registerAdmin);
 
+// user
 router.post('/users', [verifyToken, isAdmin], createUser);
 router.get('/users', [verifyToken, isAdmin], getAllUsers);
 router.get('/users/:id', [verifyToken, isAdmin], getUser);
+
+// package
 router.post('/packages', [verifyToken, isAdmin], createPackage);
+router.get('/packages', [verifyToken, isAdmin], getAllPackages);
+router.get('/packages/:id', [verifyToken, isAdmin], getPackage);
+router.put('/packages/:id', [verifyToken, isAdmin], updatePackage);
+router.delete('/packages/:id', [verifyToken, isAdmin], deletePackage);
+
+// payment
 router.post('/generate-payment', [verifyToken, isAdmin], generatePayment);
 router.get('/home', [verifyToken, isAdmin], (req, res) => {
   res.send('Hello Admin');
