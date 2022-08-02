80a2fa460949521b5c907dd16a5ffc2842e97f8f
Yogi Prasetyo <prasetyoyogi321@gmail.com>
2022-08-02 16:02:16 +0700

    update routes

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -11,10 +11,10 @@ const { verifyToken, isAdmin } = require('../middleware/authJwt');
 const router = express.Router();
 router.post('/register', registerAdmin);
 
-router.post('/create/user', [verifyToken, isAdmin], createUser);
-router.post('/create/package', [verifyToken, isAdmin], createPackage);
+router.post('/users', [verifyToken, isAdmin], createUser);
+router.post('/packages', [verifyToken, isAdmin], createPackage);
 router.post('/generate-payment', [verifyToken, isAdmin], generatePayment);
-router.get('/get-users', [verifyToken, isAdmin], getAllUser);
+router.get('/users', [verifyToken, isAdmin], getAllUser);
 router.get('/home', [verifyToken, isAdmin], (req, res) => {
   res.send('Hello Admin');
 });
